7b25dafe49d7c276ddd80a6538f7e95e6bc77db6
Daniel Sutton - BGS <dsut@bgs.ac.uk>
2022-07-01 21:40:10 +0100

    hide header when on /login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  showHead: boolean = false;
   title = 'SHiFT-Code-Redeemer-Tool';
+
+  
+  constructor(private router: Router) {
+    // on route change to '/login', set the variable showHead to false to hide header
+    router.events.forEach((event: any) => {
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/login') {
+          this.showHead = false;
+        } else {
+          this.showHead = true;
+        }
+      }
+    });
+  }
 }
