d4d4c6c9dc0276822a9c1953424068973420a255
Daniel Sutton - BGS <dsut@bgs.ac.uk>
2022-07-09 19:54:06 +0100

    add production api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { environment as baseEnvironment } from './environment.base';
 export const environment = {
   ...baseEnvironment,
   production: true,
-  apiBaseUrl: '',
+  apiBaseUrl: 'http://ec2-18-134-135-182.eu-west-2.compute.amazonaws.com:8080/borderlands-code-crawler/v1',
 };
